69d39cc451cca099d75237a7318d8d1b4ef4916a
Aiden Zepp <azepp@Elpis.local>
2022-10-31 10:46:46 +0100

    Added subtract func.

--- COP3052C/lab-06.py
+++ COP3052C/lab-06.py
@@ -2,15 +2,22 @@
 
 
 def add(x: float, y: float) -> float:
-    """Adds two numbers together."""
+    """Adds two numbers."""
 
     return x + y
+
+
+def subtract(x: float, y: float) -> float:
+    """Subtract two numbers."""
+
+    return x - y
 
 
 def main() -> None:
     x, y = 5.0, 3.0
 
     print(add(x, y))
+    print(subtract(x, y))
 
 
 if __name__ == '__main__':
